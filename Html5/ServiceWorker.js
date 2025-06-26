7965ed8deb6be6126396ea97e1465a03afaebf42
王山姆 <sakmor@gmail.com>
2025-06-26 15:42:58 +0800

    add html5

---
+++ Html5/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-WildMooseStudio-0.1";
+const contentToCache = [
+    "Build/Html5.loader.js",
+    "Build/Html5.framework.js.gz",
+    "Build/Html5.data.gz",
+    "Build/Html5.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
